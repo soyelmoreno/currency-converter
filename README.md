4a08e4b9423ef76a9238d05b7f93b69e7b6b8b35
Carlos Moreno <soyelmoreno@yahoo.com>
2020-10-19 14:47:35 -0700

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-# currency-converter
-
-Currency Converter is a small tool for converting and calculating between
-various crytocurrencies. It's written in React.
-
-https://soyelmoreno.github.io/currency-converter/
---
+++ precache-manifest.6fb4fa7bc9dc48cda97146c283414342.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d5f511152918a60b1c1dda450312195",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "5ffae50a647a51cb1949",
+    "url": "/currency-converter/static/css/main.361a27ef.chunk.css"
+  },
+  {
+    "revision": "8517cfd58c1515bdfe2c",
+    "url": "/currency-converter/static/js/2.a11c7d0e.chunk.js"
+  },
+  {
+    "revision": "5ffae50a647a51cb1949",
+    "url": "/currency-converter/static/js/main.02927985.chunk.js"
+  },
+  {
+    "revision": "768849e9ea24e042b723",
+    "url": "/currency-converter/static/js/runtime-main.acf7e268.js"
+  }
+]);
