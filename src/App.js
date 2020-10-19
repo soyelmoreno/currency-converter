e497a12d5a9ca4c0a6f36a84b353afdd548d6cc8
Carlos Moreno <soyelmoreno@yahoo.com>
2020-10-18 21:05:54 -0700

    - Add styles for purple background, orange border, and form controls - Add select-css library for styling dropdowns

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import './select-css.css';
 
 function App() {
   return (
@@ -8,36 +9,40 @@ function App() {
         <h1>Currency Converter</h1>
       </header>
 
-      <main className="main">
-        <section>
-          <h2>Currencies</h2>
+      <main className="App-main">
+        <section className="App-section">
+          <h2 className="visually-hidden">Choose currencies</h2>
 
           <div className="cols controls">
             {/* Currency 1 select */}
             <div className="col">
-              <label htmlFor="curr1">Currency 1</label>
-              <select>
-                <option>Select...</option>
-                <option>USD</option>
-                <option>EUR</option>
-                <option>GBP</option>
-              </select>
+              <div className="control">
+                <label htmlFor="curr1">Currency 1</label>
+                <select className="select-css">
+                  <option>Select...</option>
+                  <option>USD</option>
+                  <option>EUR</option>
+                  <option>GBP</option>
+                </select>
+              </div>
             </div>
 
             {/* Swap button */}
             <div className="col">
-              <button>{`< Swap >`}</button>
+              <button className="btn-swap">{`< Swap >`}</button>
             </div>
 
             {/* Currency 2 select */}
             <div className="col">
-              <label htmlFor="curr2">Currency 1</label>
-              <select>
-                <option>Select...</option>
-                <option>USD</option>
-                <option>EUR</option>
-                <option>GBP</option>
-              </select>
+              <div className="control">
+                <label htmlFor="curr2">Currency 2</label>
+                <select className="select-css">
+                  <option>Select...</option>
+                  <option>USD</option>
+                  <option>EUR</option>
+                  <option>GBP</option>
+                </select>
+              </div>
             </div>
           </div>
 
@@ -48,24 +53,46 @@ function App() {
           </div>
         </section>
 
-        <section>
-          <h2>Amounts</h2>
+        <section className="App-section">
+          <h2 className="visually-hidden">Enter amounts</h2>
 
           <div className="cols controls">
             {/* Amount 1 */}
             <div className="col">
-              <label htmlFor="amount1">Amount</label>
-              <input type="text" id="amt1" placeholder="Enter an amount" />
+              <div className="control">
+                <label htmlFor="amount1">Amount of currency 1</label>
+                <div className="input-wrap">
+                  <input
+                    type="text"
+                    id="amt1"
+                    name="amt1"
+                    className="input-amt"
+                    placeholder="Enter an amount"
+                  />
+                  <span>{`USD`}</span>
+                </div>
+              </div>
             </div>
 
             {/* Amount 2 */}
             <div className="col">
-              <label htmlFor="amount2">Amount</label>
-              <input type="text" id="amt2" placeholder="Enter an amount" />
+              <div className="control">
+                <label htmlFor="amount2">Amount of currency 2</label>
+                <div className="input-wrap">
+                  <input
+                    type="text"
+                    id="amt2"
+                    name="amt2"
+                    className="input-amt"
+                    placeholder="Enter an amount"
+                  />
+                  <span>{`EUR`}</span>
+                </div>
+              </div>
             </div>
             {/* Display equations */}
             <div className="display">
-              <p className="equation">5 USD = 11.25 EUR</p>
+              <p className="equation calculated">5 USD = 11.25 EUR</p>
             </div>
           </div>
         </section>
